91922546655c8a17c7ae78bb999d4cea4e468a95
boffu163 <sashalavrenchuk642@gmail.com>
2024-03-13 20:32:29 +0500

    change js,css,html

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Здесь будет описан весь скрипт проекта //
+
+let btn_main = document.querySelector('.main-top-row')
+let btn_stat = document.querySelector('.stat-top-row')
+let btn_game = document.querySelector('.game-top-row')
+let btn_user = document.querySelector('.img-user-male')
