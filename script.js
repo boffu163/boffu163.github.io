00be36a2cadd87c2bad977043b9e6d9677a9eb54
boffu163 <sashalavrenchuk642@gmail.com>
2024-03-16 21:43:25 +0500

    redesigned the script, added the html and css structure

--- script.js
+++ script.js
@@ -2,73 +2,39 @@
 
 stat_page = document.querySelector('.statistics-page')
 btn_stat = document.querySelector('.stat-top-row')
+main_page = document.querySelector('.main-page')
+btn_main = document.querySelector('.main-top-row')
+game_page = document.querySelector('.your-game-page')
+btn_game = document.querySelector('.game-top-row')
+account_page = document.querySelector('.your-account-page')
+btn_account = document.querySelector('.img-user-male')
+
+
+
 btn_stat.addEventListener('click', function(){
     stat_page.style.display = 'flex'
     main_page.style.display = 'none'
+    game_page.style.display = 'none'
+    account_page.style.display = 'none'
 })
 
-
-
-main_page = document.querySelector('.main-page')
-btn_main = document.querySelector('.main-top-row')
 btn_main.addEventListener('click', function(){
-    main_page.style.display = 'block'
+    main_page.style.display = 'flex'
     stat_page.style.display = 'none'
-    your_game_page.style.display = 'none'
-})
-btn_main1 = document.querySelector('.main-top-row-link')
-btn_main1.addEventListener('click', function(){
-    main_page.style.display = 'block'
-    stat_page.style.display = 'none'
+    game_page.style.display = 'none'
+    account_page.style.display = 'none'
 })
 
-
-
-your_game_page = document.querySelector('.your-game-page')
-btn_your_game = document.querySelector('.game-top-row')
-btn_your_game.addEventListener('click', function(){
+btn_game.addEventListener('click', function(){
+    game_page.style.display = 'flex'
     main_page.style.display = 'none'
     stat_page.style.display = 'none'
-    your_game_page.style.display = 'flex'
-})
-btn_main2 = document.querySelector('.main-top-row-link2')
-btn_main2.addEventListener('click', function(){
-    main_page.style.display = 'block'
-    your_game_page.style.game = 'none'
-})
-btn_your_game1 = document.querySelector('.game-top-row-link')
-btn_your_game1.addEventListener('click', function(){
-    stat_page.style.display = 'none'
-    your_game_page.style.display = 'flex'
-})
-btn_stat1 = document.querySelector('.stat-top-row-link')
-btn_stat1.addEventListener('click', function(){
-    stat_page.style.display = 'flex'
-    your_game_page.style.display = 'none'
-})
-btn_your_account_page1 = document.querySelector('.game-top-row-link2')
-btn_your_account_page.addEventListener('click', function(){
-    your_game_page.style.display = 'none'
-    your_account_page.style.display = 'flex'
+    account_page.style.display = 'none'
 })
 
-
-
-your_account_page = document.querySelector('.your-account-page')
-btn_your_account_page = document.querySelector('.img-user-male')
-btn_your_account_page.addEventListener('click', function(){
+btn_account.addEventListener('click', function(){
+    account_page.style.display = 'flex'
+    game_page.style.display = 'none'
     main_page.style.display = 'none'
     stat_page.style.display = 'none'
-    your_game_page.style.display = 'none'
-    your_account_page.style.display = 'flex'
-})
-btn_main3 = document.querySelector('.main-top-row-link3')
-btn_main3.addEventListener('click', function(){
-    main_page.style.display = 'block'
-    your_account_page.style.game = 'none'
-})
-btn_stat2 = document.querySelector('.stat-top-row-link1')
-btn_stat2.addEventListener('click', function(){
-    stat_page.style.display = 'flex'
-    your_account_page.style.display = 'none'
 })
